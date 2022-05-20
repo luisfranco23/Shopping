e06df27ef35f54f5b0f7aa3903ab0872ff08f484
Luis Franco <lafrancol@unitecnica.net>
2022-05-19 23:19:44 -0500

    agregando css

--- assets/js/main.js
+++ assets/js/main.js
@@ -30,59 +30,68 @@ navbarBtn.addEventListener('click', function (e) {
 const products = [
   {
     id: 1,
-    name: 'Product 1',
-    price: 100,
-    image: 'https://picsum.photos/250/250?random=1',
-    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quidem.'
+    name: 'Lenovo Legion 5 Pro (16" AMD)',
+    price: 7799001,
+    image: 'https://http2.mlstatic.com/D_NQ_NP_638942-MLA49937651999_052022-V.jpg',
+    description: `Rendimiento épico por dentro y por fuera
+    Procesamiento AMD Ryzen™ y tarjeta gráfica NVIDIA® GeForce RTX™
+    Primera laptop de 16” del mundo con hasta 165 Hz
+    Tecnología de audio 3D de Nahimic: podrás ver y oír a cualquier enemigo acercándose a ti.`
   },
   {
     id: 2,
-    name: 'Product 2',
-    price: 200,
-    image: 'https://picsum.photos/250/250?random=2',
-    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quidem.'
+    name: 'Lenovo Yoga Slim 7 Pro',
+    price: 7500200,
+    image: 'https://i.blogs.es/51e72d/lenovo/1366_2000.jpg',
+    description: `AMD Ryzen 5 4600H, el Ryzen 7 4800H o el Ryzen 9 4900H. La gráfica
+    RAM serán de 8 o 16 GB Dual Channel, mientras que el almacenamiento interno irá desde 256 GB a 1 TB SSD PCIe M.2.`
   },
   {
     id: 3,
-    name: 'Product 3',
-    price: 300,
-    image: 'https://picsum.photos/250/250?random=3',
-    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quidem.'
+    name: 'Alienware Laptop',
+    price: 8500212,
+    image: 'https://www.pngarts.com/files/12/Alienware-Laptop-PNG-Pic.png',
+    description: `Está equipado con la última generación de procesadores Intel Core i7 con hasta 8 GB de RAM y 1 TB en formato de disco duro.`
   },
   {
     id: 3,
-    name: 'Product 4',
-    price: 300,
-    image: 'https://picsum.photos/250/250?random=4',
-    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quidem.'
+    name: 'LENOVO YOGA BOOK',
+    price: 4769331,
+    image: 'https://www.tuexperto.com/wp-content/uploads/2018/09/lenovo-yoga-s730-02.jpg',
+    description: `En su interior encontramos un procesador Intel Atom  X5 – Z8550 de cuatro núcleos, acompañado de 4 GB de RAM y 64 GB de almacenamiento interno (ampliables mediante tarjeta microSD).`
   },
   {
     id: 3,
-    name: 'Product 5',
-    price: 300,
-    image: 'https://picsum.photos/250/250?random=5',
-    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quidem.'
+    name: 'LENOVO THINKPAD X1 YOGA',
+    price: 8460012,
+    image: 'https://www.tuexperto.com/wp-content/uploads/2018/09/captura-de-pantalla-2018-09-12-a-las-12-45-03.png',
+    description: ` El portátil también cuenta con un panel de 14 pulgadas con una resolución de hasta WQHD o 2.560 x 1.440 pixeles. No falta reconocimiento facial para aumentar la seguridad o una batería para hasta 12 horas`
   },
   {
     id: 3,
-    name: 'Product 6',
-    price: 300,
-    image: 'https://picsum.photos/250/250?random=6',
-    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quidem.'
+    name: 'Lenovo Laptop S145',
+    price: 5894002,
+    image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR9A5nJ1PpZfvWutOgDrYQIu8Wh_4cWK0AnZzoDouGOh2fzt6GPphUvHTU1Z8yBmTHCTuQ&usqp=CAU',
+    description: `Laptop Lenovo Procesador AMD 4gb Ram 500gb HDD modelo S145 - 14 Pulgadas`
   },
   {
     id: 3,
-    name: 'Product 7',
-    price: 300,
-    image: 'https://picsum.photos/250/250?random=7',
-    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quidem.'
+    name: 'Lenovo Yoga Slim 7',
+    price: 6500323,
+    image: 'https://files.lafm.com.co/assets/public/styles/i_a_750x375/public/2021-05/lenovo-yoga-7_0.jpg?_5CZNbHTHlej7StHU.rPJi4WtKPspKN_&itok=KTOtosae',
+    description: `Procesador: Intel Core i7 10 generación
+    Tarjeta Gráfica: integrada Intel Iris Plus
+    Memoria RAM: 16 GB LPDDR4X
+    Almacenamiento: 512 GB SSD`
   },
   {
     id: 3,
-    name: 'Product 8',
-    price: 300,
-    image: 'https://picsum.photos/250/250?random=8',
-    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quidem.'
+    name: 'ThinkPad X1 Carbon Gen 9',
+    price: 7840022,
+    image: 'https://s1.eestatic.com/2021/01/11/omicrono/hardware/550456629_169713586_1024x576.jpg',
+    description: `Procesadores hasta Intel® Core™ i7 de 11va generación y Evo® opcional
+    Chasis ultradelgado y ultraliviano, y una batería de mayor duración
+    Impresionante pantalla de 14” 16:10, opcional táctil en algunos modelos`
   }
 ]
 
